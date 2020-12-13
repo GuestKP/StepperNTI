7f14dc0ce4e849d3be83b6a95636af8a60010afe
GuestKP_mainPC <72121410+GuestKP@users.noreply.github.com>
2020-12-14 00:02:22 +0700

    Add func + update moveSteps
    Added angle and linear move func declaration
    Updated moveSteps: added steps division
    Minor updates of other func

--- StepperNTI.cpp
+++ StepperNTI.cpp
@@ -1,13 +1,15 @@
 #include "StepperNTI.h"
-
+#include <cmath>
 
 Stepper::Stepper(int pin_dir, int pin_step, int pin_m1, int pin_m2, int pin_m3)
 {
+	// save pins
 	Stepper::pin_dir = pin_dir;
 	Stepper::pin_step = pin_step;
 	Stepper::pin_m1 = pin_m1;
 	Stepper::pin_m2 = pin_m2;
 	Stepper::pin_m3 = pin_m3;
+	// configure pins
 	pinMode(pin_dir, OUTPUT);
 	pinMode(pin_step, OUTPUT);
 	pinMode(pin_m1, OUTPUT);
@@ -16,13 +18,16 @@ Stepper::Stepper(int pin_dir, int pin_step, int pin_m1, int pin_m2, int pin_m3)
 }
 
 
-void Stepper::initLinear(int new_mpr)
+void Stepper::initLinear(float new_mpr)
 {
-	mm_per_revolution = new_mpr;
-	linear = true;
+	if(new_mpr > 0)
+	{
+		mm_per_revolution = new_mpr;
+		flag_linear = true;
+	}
 }
 
-void Stepper::initLinear(int n_teeth, int tooth_width)
+void Stepper::initLinear(int n_teeth, float tooth_width)
 {
 	initLinear(n_teeth*tooth_width);
 }
@@ -30,28 +35,7 @@ void Stepper::initLinear(int n_teeth, int tooth_width)
 
 void Stepper::setSPR(int new_spr)
 {
-	step_per_revolution = new_spr;
-}
-
-
-void Stepper::moveStep(long steps)
-{
-	digitalWrite(pin_dir, ((steps > 0) == move_side));
-	steps *= divide_koeff;
-	int cur_hst = half_step_time / divide_koeff;
-	for(int i=0; i<steps; i++)
-	{
-		digitalWrite(pin_step, 1);
-		delayMicroseconds(cur_hst);
-		digitalWrite(pin_step, 0);
-		delayMicroseconds(cur_hst);
-	}
-}
-
-
-void Stepper::moveAngle(long angle)
-{
-	
+	if(new_spr > 0) step_per_revolution = new_spr;
 }
 
 
@@ -60,12 +44,17 @@ int Stepper::setDivision(int div)
 	if((div == 1) || (div == 2) || (div == 4) || (div == 8) || (div == 16) || (div == 32))
 	{
 		divide_koeff = div;
+		
+		// power levels on pins M1, M2, M3 is binary representation
+		// of 'n', where 2^n - number of microsteps in step, so:
+		// find 'n'
 		n = 0;
-		while(div > 1) // get n, where 2^n == div
+		while(div > 1)
 		{
 			div /= 2;
 			n += 1;
 		}
+		// write bits of 'n'
 		digitalWrite(pin_m1, n & 0x1);
 		digitalWrite(pin_m2, n & 0x2);
 		digitalWrite(pin_m3, n & 0x4);
@@ -77,6 +66,71 @@ int Stepper::setDivision(int div)
 		return 0;
 	}
 
+}
+
+
+void Stepper::moveSteps(float steps)
+{
+	int msteps_32 = (abs(steps) % (1/divide_koeff)) * 32;
+	int msteps_k = abs(steps) * divide_koeff;
+	
+	int cur_hst = half_step_time / divide_koeff;
+	
+	// set move direction
+	digitalWrite(pin_dir, ((steps > 0) == move_side));
+	
+	// there must be acceleration
+	
+	// 'main' move
+	for(int i=0; i<int(msteps_k); i++)
+	{
+		digitalWrite(pin_step, 1);
+		delayMicroseconds(cur_hst);
+		digitalWrite(pin_step, 0);
+		delayMicroseconds(cur_hst);
+	}
+	if(msteps_32)
+	{
+		int tmp_dk = divide_koeff;
+		setDivision(32);
+		for(int i=0; i<int(steps); i++)
+		{
+			digitalWrite(pin_step, 1);
+			delayMicroseconds(cur_hst);
+			digitalWrite(pin_step, 0);
+			delayMicroseconds(cur_hst);
+		}
+		setDivision(tmp_dk);
+	}
+	
+	// there must be deceleration
+	
+	pos_linear += steps * 32;
+	pos_angle = (pos_angle + steps * 32) % (step_per_revolution * 32);
+}
+
+
+void Stepper::moveAngleRel(float angle)
+{
+	
+}
+
+
+void Stepper::moveAngleAbs(float angle)
+{
+	
+}
+
+
+void Stepper::moveLinearRel(float mm)
+{
+	
+}
+
+
+void Stepper::moveLinearAbs(float mm)
+{
+	
 }
 
 
--- StepperNTI.h
+++ StepperNTI.h
@@ -7,27 +7,33 @@
 	#include "WProgram.h"
 #endif
 
-#define MAX_HALF_STEP_TIME 448
+#define MIN_HALF_STEP_TIME 448
 #define DEFAULT_SPR 200
 
 class Stepper
 {
 	private:
 		int pin_dir, pin_step, pin_m1, pin_m2, pin_m3,
-			divide_koeff = 1,
-			mm_per_revolution = DEFAULT_SPR,
-			half_step_time = MAX_HALF_STEP_TIME;
-		float mm_per_step;
-		bool move_side = true, linear = false;
+			step_per_revolution = DEFAULT_SPR,
+			half_step_time = MIN_HALF_STEP_TIME,
+			mult_koeff = 32, divide_koeff = 1,
+			acceleration = 0;
+		// angle and linear position units is STEPS*32, due to loss of float numbers
+		long pos_linear = 0, pos_angle = 0;
+		float mm_per_revolution;
+		bool move_side = true, flag_linear = false, flag_acceleration = false;
 		
 	public:
 		Stepper(int pin_dir, int pin_step, int pin_m1, int pin_m2, int pin_m3);
 		void initLinear(int mm_per_rev);
 		void initLinear(int n_teeth, int tooth_width);
 		void setSPR(int new_spr);
-		void moveStep(long steps);
-		void moveAngle(long deg);
 		int setDivision(int div);
+		void moveSteps(float steps);
+		void moveAngleRel(float angle);
+		void moveAngleAbs(float angle);
+		void moveLinearRel(float mm);
+		void moveLinearAbs(float mm);
 };
 
 #endif
